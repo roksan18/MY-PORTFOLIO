035d69506c5ec078239bb59d5b0dd4582d9e2a48
Rose Ann <rasbajo.18@gmail.com>
2025-12-28 23:15:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/MY-PORTFOLIO/",
-=======
-  base: "MY-PORTFOLIO",
->>>>>>> e7109b8 (Fix project image paths for GitHub Pages)
 });
